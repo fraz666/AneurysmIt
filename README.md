ef31ab53a64c94575bbf97d52f48fe101e4d3785
fraz666 <fraz666@blu.it>
2022-11-06 23:22:00 +0000

    init code

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # AneurysmIt
 Web image viewer with "brain aneurysm" sound player
+
+## Development environment
+1. Create a Dockerfile containing the required tools
+
+    ```Dockerfile
+    FROM alpine:3
+    RUN apk update
+
+    # TOOLS
+    RUN apk add nodejs npm git
+
+    WORKDIR /app
+    ```
+
+2. Build it and name it 
+    ```
+    docker build -t <dev-env name> .
+    ```
+
+3. Run it 
+    ```
+    docker run --rm -it -p 3000:3000 -v ${PWD}/:/app <dev-env name> ash
+    ```
+
+    Thanks to volume binding it's possible to edit the local files and have them updated inside the container.
+
+## Using vite and svelte
+
+1. Scaffolding via a vite template
+    ```
+    npm create vite@latest src -- --template svelte-ts
+    ```
+
+2. Replacing the dev script in package.json
+    ```json
+    "dev": "vite --host 0.0.0.0 --port 3000"
+    ```
+
+3. Starting the dev environment
+    ```
+    npm i & npm run dev
+    ```
+
---
+++ src/src/App.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import audioSrc from "./assets/audio.mp3";
+
+  const audio = new Audio(audioSrc);
+
+  let isZoomed = false;
+
+  function playAnimation() {
+    audio.play();
+    isZoomed = !isZoomed;
+  }
+</script>
+
+<main>
+  <div class="meme" class:zoomed={isZoomed}>
+    <button on:click={playAnimation}> Click me </button>
+  </div>
+</main>
+
+<style>
+  button {
+    background-color: transparent;
+    color: var(--primary);
+    font-size: 18px;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    display: inline-block;
+    text-align: center;
+    width: 10em;
+    font-weight: bold;
+    padding: 1em 0;
+    border-radius: .25em;
+    border: .15em solid;
+    text-decoration: none;
+    -webkit-transition: 0.3s all ease;
+    transition: 0.3s ease all;
+    
+  }
+
+  button:hover {
+    background-color: var(--primary);
+    color: #fff;
+    border: .15em var(--primary);
+  }
+
+  .meme {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+
+    background-image: url("image.jpg");
+    background-position: center;
+    background-size: contain;
+    height: 100vh;
+    width: 100vw;
+    transition: transform 2s;
+  }
+
+  .zoomed {
+    transform: scale(1.3);
+  }
+</style>
